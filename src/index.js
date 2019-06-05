1f98076d5dd9cba1712e7f47f1ec47b2fa0386ef
Roger Sobrado <rsobrado@goduesouth.com>
2019-06-04 20:11:11 -0600

    tech stack added

--- src/index.js
+++ src/index.js
@@ -14,72 +14,70 @@ import './sass/main.scss'
 import projects from './data/projects.json'
 import experiments from './data/experiments.json'
 
-let projectRow  = 0;
-let experimentRow = 0;
-
 const projectsContainer = document.getElementById('projects');
 const experimentsContainer = document.getElementById('experiments');
 
 var createNode = (element) => {return document.createElement(element)}
 var appendNode = (parent, element) => {return parent.appendChild(element)}
 
-var loadProjects = () =>{
-  
-  projects.map(function(project) { // Map through the results and for each run the code below
+var loadCards = (elem, cardContainer) =>{
+  let experimentRow = 0;
+  elem.map(function(elem) { // Map through the results and for each run the code below
+    
     let col = createNode('div'), //  Create the elements we need
       card = createNode('div'),
       img = createNode('img'),
-      title = createNode('h2')
+      desc = createNode('p'),
+      linkedImg = createNode('a'),
+      viewLive = createNode('span'),
+      title = createNode('h2'),
+      overlay = createNode('div'),
+      stack = createNode('div')
       
-    card.setAttribute('class', 'card')
+    card.setAttribute('class', 'card') //  set attributes to the elements created
+    overlay.setAttribute('class', 'description')
     title.setAttribute('class', 'title')
-    img.src = project.thumb  // Add the source of the image to be the src of the img element
-    title.innerHTML = `<a href="${project.url}" target="_blank">${project.name} </a>` // Make the HTML of our span to be the first and last name of our author
+    img.src = elem.thumb
+    img.setAttribute('alt', elem.name) 
+    linkedImg.appendChild(img)
+    linkedImg.setAttribute('href', elem.url)
+    linkedImg.setAttribute('target', "_blank")
+    title.innerHTML = `<a href="${elem.url}" target="_blank">${elem.name} </a>` 
+    desc.innerHTML = elem.desc 
+    viewLive.innerHTML = `<a href="${elem.url}" target="_blank">View Live </a>` 
+    img.innerHTML = `<a href="${elem.url}" target="_blank">${img} </a>` 
 
-    if((projectRow==0)||(projectRow==3)){
+
+    if((experimentRow==0)||(experimentRow==3)){ // create the grid according to index
       col.setAttribute('class', 'col-md-12 col-xs-12')
     }
     else{
       col.setAttribute('class', 'col-md-6 col-xs-12')
     }
-    
-    appendNode(card, img)
-    appendNode(card, title)
-    appendNode(col, card)
-    projectRow++
-    return appendNode(projectsContainer, col)
-  })
-}
 
-var loadExperiments = () =>{
-  experiments.map(function(experiment) { // Map through the results and for each run the code below
-    let col = createNode('div'), //  Create the elements we need
-      card = createNode('div'),
-      img = createNode('img'),
-      title = createNode('h2')
-      
-    card.setAttribute('class', 'card')
-    title.setAttribute('class', 'title')
-    img.src = experiment.thumb  // Add the source of the image to be the src of the img element
-    img.setAttribute('alt', experiment.name) // Add alternative texte to the image to the img element
-    title.innerHTML = `<a href="${experiment.url}" target="_blank">${experiment.name} </a>` // Make the HTML of our span to be the first and last name of our author
-
-  
-    if((experimentRow==0)||(experimentRow==3)){
-      col.setAttribute('class', 'col-md-12 col-xs-12')
-    }
-    else{
-      col.setAttribute('class', 'col-md-6 col-xs-12')
-    }
-    
-    appendNode(card, img)
-    appendNode(card, title)
+    appendNode(overlay, desc)
+    appendNode(overlay, loadTech(elem.stack))
+    appendNode(overlay, viewLive)
+    appendNode(card, title) //  Attach element to the card
+    appendNode(card, linkedImg)
+    appendNode(card, overlay)
     appendNode(col, card)
     experimentRow++
-    return appendNode(experimentsContainer, col)
+    return appendNode(cardContainer, col)
   })
 }
 
-        
-loadProjects();
-loadExperiments();
+var loadTech = (stack) => {
+  let i = 0,
+    tech = createNode('div')
+  
+  tech.setAttribute('class', 'techStack') 
+  
+  for(i = 0; i < stack.length; i++){
+    tech.innerHTML +=  `<span class="badge badge-info">${stack[i]} </span>` 
+  }
+  return tech
+}
+
+loadCards(projects, projectsContainer);
+loadCards(experiments, experimentsContainer);
